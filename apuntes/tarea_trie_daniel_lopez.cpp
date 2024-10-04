23c2355409ac261bdbf49d2ae90824258ac8f6f8
Danilepez <danilepez5@gmail.com>
2024-10-04 18:47:39 -0400

    cambios kruskal2

--- apuntes/tarea_trie_daniel_lopez.cpp
+++ apuntes/tarea_trie_daniel_lopez.cpp
@@ -41,7 +41,7 @@ bool search(node *trie, string word) {
     return currentNode->endOfWord;  
 }
 
-bool deleteWord(node *trie, string word) {
+/*bool deleteWord(node *trie, string word) {
     node *currentNode = trie;
     for (int i = 0; i < word.size(); i++) {
         int index = word[i] - 'a';
@@ -58,6 +58,48 @@ bool deleteWord(node *trie, string word) {
         return true;  
     }
     return false;  
+}*/
+bool isEmpty(node* currentNode) {
+    for (int i = 0; i < 26; i++) {
+        if (currentNode->children[i] != NULL) {
+            return false; // El nodo tiene al menos un hijo
+        }
+    }
+    return true;
+}
+
+// Función para eliminar una palabra del trie
+node* removeWord(node* trie, const std::string& word, int depth = 0) {
+    // Si el trie está vacío
+    if (!trie)
+        return NULL;
+
+    // Si se está procesando el último carácter de la palabra
+    if (depth == word.size()) {
+        // Ya no es fin de palabra después de la eliminación
+        if (trie->endOfWord)
+            trie->endOfWord = false;
+
+        // Si el nodo no es prefijo de ninguna otra palabra
+        if (isEmpty(trie)) {
+            delete trie;
+            trie = NULL;
+        }
+
+        return trie;
+    }
+
+    // Si no es el último carácter, proceder recursivamente
+    int index = word[depth] - 'a';
+    trie->children[index] = removeWord(trie->children[index], word, depth + 1);
+
+    // Si el nodo actual no tiene hijos y no es el fin de otra palabra
+    if (isEmpty(trie) && !trie->endOfWord) {
+        delete trie;
+        trie = NULL;
+    }
+
+    return trie;
 }
 
 int main() {
@@ -73,6 +115,6 @@ int main() {
     cout << (search(trie, "apricot") ? "Si existe la palabra 'apricot'" : "No existe la palabra 'apricot'") << endl;
 
     // Eliminar palabras
-    cout << (deleteWord(trie, "apple") ? "Se eliminó la palabra 'apple'" : "No se pudo eliminar la palabra 'apple'") << endl;
+    cout << (removeWord(trie, "apple") ? "Se eliminó la palabra 'apple'" : "No se pudo eliminar la palabra 'apple'") << endl;
     cout << (search(trie, "apple") ? "Si existe la palabra 'apple'" : "No existe la palabra 'apple'") << endl;
 }
