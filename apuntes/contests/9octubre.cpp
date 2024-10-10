7d285b4a6cde529a6f5952b01ecaa92ba4382141
Danilepez <danilepez5@gmail.com>
2024-10-09 23:32:45 -0400

    contest fight2

--- apuntes/contests/9octubre.cpp
+++ apuntes/contests/9octubre.cpp
@@ -10,49 +10,11 @@ using namespace std;
 #define output freopen("out.txt", "w", stdout)
 using namespace std;
 
-int battle(vector<int>& ratings) {
-    while (ratings.size() > 1) {
-        if (ratings.size() % 2 == 0) {
-            // Para tamaño par, restar los elementos del medio
-            int aux = ratings[ratings.size() / 2] - ratings[ratings.size() / 2 - 1];
-            ratings.erase(ratings.begin() + ratings.size() / 2 - 1, ratings.begin() + ratings.size() / 2 + 1); // Borra los dos elementos del medio
-            ratings.insert(ratings.begin() + ratings.size() / 2, aux);  // Inserta el nuevo valor
-        } else {
-            // Para tamaño impar, restar el primer y el penúltimo
-            int aux = ratings[ratings.size() - 2] - ratings[0];
-            ratings.erase(ratings.begin());  // Borra el primer elemento
-            ratings.erase(ratings.end() - 2);  // Borra el penúltimo elemento
-            ratings.insert(ratings.begin() + ratings.size() - 1, aux);  // Inserta el nuevo valor
-        }
-    }
-    
-    return ratings[0];
-}
-
-int main() {
-    int n_cases;
-    cin >> n_cases;
-
-    while (n_cases--) {
-        int fighters;
-        cin >> fighters;
-        vector<int> ratings(fighters);
-
-        for (int i = 0; i < fighters; i++) {
-            cin >> ratings[i];
-        }
-
-        cout << battle(ratings) << endl;  // Mostrar el resultado de la batalla
-    }
-
-    return 0;
-}
-
 
 
 //Recursividad 
 
-/*int battle(vector<int> ratings) {
+int battle(vector<int> ratings) {
     if (ratings.size() == 1) {
         return ratings[0];
     } 
@@ -108,4 +70,4 @@ int main(){
         cout<<battle(ratings)<<endl;
     }
     return 0;
-}*/
+}
